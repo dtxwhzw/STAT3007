499266e3243d13369d0c59c539f969307bfa7ce9
zhiwei <z.hu1@uqconnect.edu.au>
2021-05-12 15:51:51 +0800

    generate file finish the model and dataset

--- dataset.py
+++ dataset.py
@@ -4,23 +4,56 @@ import torch
 from torch.utils.data.dataset import Dataset
 
 
-class BaseDataset(Dataset):
-    def __init__(self):
+class BaseDataset(Dataset) :
+    def __init__(self) :
         super(BaseDataset, self).__init__()
 
-    def __len__(self):
+    def __len__(self) :
         raise NotImplemented
 
-    def __getitem__(self, item):
-        if torch.is_tensor(item):
+    def __getitem__(self, item) :
+        if torch.is_tensor(item) :
             item = item.to_list()
-        if isinstance(item, slice):
+        if isinstance(item, slice) :
             begin, end, step = item.indices(len(self))
-            return [self.get_example(i) for i in range(begin,end,step)]
-        if isinstance(item, list):
+            return [self.get_example(i) for i in range(begin, end, step)]
+        if isinstance(item, list) :
             return [self.get_example(i) for i in item]
-        else:
+        else :
             return self.get_example(item)
 
-    def get_example(self,item):
+    def get_example(self, item) :
         raise NotImplemented
+
+
+class SentimentDataset(BaseDataset):
+    def __init__(self,tokenizer,texts,labels,max_len):
+        super(SentimentDataset, self).__init__()
+        self.tokenizer = tokenizer
+        self.texts = texts
+        self.labels = labels
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, item):
+        text = self.texts[item]
+        label = self.labels[item]
+
+        encoding = self.tokenizer.encode_plus(
+            text,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            return_token_type_ids=False,
+            pad_to_max_length=True,
+            return_attention_mask=True,
+            return_tensors='pt'
+        )
+
+        return {
+            'text':text,
+            'input_ids':encoding['input_ids'].flatten(),
+            'attention_mask':encoding['attention_mask'].flatten(),
+            'labels': torch.tensor(label,dtype=torch.long)
+        }
--- model.py
+++ model.py
@@ -3,6 +3,7 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
+from transformers import BertModel
 
 
 class BaseModel(nn.Module):
@@ -11,3 +12,24 @@ class BaseModel(nn.Module):
 
     def forward(self, x, *wargs, **kwargs):
         raise NotImplemented
+
+
+class BertClassifier(BaseModel):
+    def __init__(self,conf):
+        super(BertClassifier, self).__init__()
+        self.conf = conf
+        self.bert_path = getattr(conf,'bert_path',None)
+        pretrain_name = 'bert-base-cased'
+        if self.bert_path:
+            pretrain_name = self.bert_path
+        print(f'Bert Model from: {self.bert_path}')
+        self.bert = BertModel.from_pretrained(pretrain_name)
+        self.drop = nn.Dropout(p=0.3)
+        self.classifier = nn.Linear(self.bert.config.hidden_size,conf.class_num)
+
+    def forward(self,input_ids, attention_mask):
+        outputs = self.bert(input_ids, attention_mask = attention_mask),
+        activated_cls = outputs[1]
+        activated_cls = self.drop(activated_cls)
+        logits = self.classifier(activated_cls)
+        return logits
